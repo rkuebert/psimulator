002f4db3e4f570faaac8a10afd14a3482db52c46
Roland Kuebert <roland.kuebert@gmail.com>
2016-08-18 17:42:03 +0200

    Updated javadoc.

--- shared/src/main/java/shared/Components/AbstractComponentModel.java
+++ shared/src/main/java/shared/Components/AbstractComponentModel.java
@@ -3,34 +3,40 @@ package shared.Components;
 import java.io.Serializable;
 
 /**
- *
+ * An abstract component consisting of an id and the component's
+ * hardware type.
+ * 
  * @author Martin Švihlík <svihlma1 at fit.cvut.cz>
  */
-public interface AbstractComponentModel extends Identifiable, Serializable{
-    
+public interface AbstractComponentModel extends Identifiable, Serializable {
+
     /**
-     * Returns unique ID
-     * @return 
+     * Returns the components id.
+     *
+     * @return the components id
      */
     @Override
     public Integer getId();
     
     /**
-     * Returs HwType of component
-     * @return 
+     * Sets the unique identifier of the component.
+     *
+     * @param id the unique identifier of the component
+     */
+    public void setId(Integer id);
+
+    /**
+     * Returns the hardware type of the component.
+     *
+     * @return the hardware type of the component
      */
     public HwTypeEnum getHwType();
 
     /**
-     * Sets HwType of component
-     * @param hwType 
+     * Sets the hardware type of the component.
+     *
+     * @param hwType the hardware type of the component
      */
     public void setHwType(HwTypeEnum hwType);
 
-    /**
-     * Gets unique identifier of component
-     * @return 
-     */
-    public void setId(Integer id);
-    
 }
--- shared/src/main/java/shared/Components/Identifiable.java
+++ shared/src/main/java/shared/Components/Identifiable.java
@@ -1,13 +1,16 @@
 package shared.Components;
 
 /**
- *
+ * Interface for identifiable entities.
+ * 
  * @author Martin Švihlík <svihlma1 at fit.cvut.cz>
  */
 public interface Identifiable {
+
     /**
-     * Gets unique identifier of component
-     * @return 
+     * Returns the components id.
+     *
+     * @return the components id
      */
     public Integer getId();
 }
