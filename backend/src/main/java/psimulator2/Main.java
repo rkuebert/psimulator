00de81d4bd6dfa162f189e92b256671367035307
Roland Kuebert <roland.kuebert@gmail.com>
2016-08-18 17:49:03 +0200

    Source code formatting.

--- backend/src/main/java/psimulator2/Main.java
+++ backend/src/main/java/psimulator2/Main.java
@@ -11,6 +11,7 @@ import java.util.Date;
 import logging.Logger;
 import logging.LoggingCategory;
 import logging.networkEvents.EventServer;
+import shared.Components.HwComponentModel;
 import shared.Components.NetworkModel;
 import shared.Serializer.AbstractNetworkSerializer;
 import shared.Serializer.NetworkModelSerializerXML;
@@ -36,11 +37,13 @@ public class Main {
     public static void main(String[] args) {
 
         // TODO Generate build information via gradle - this is useless
-        System.out.println("Starting Psimulator2, build " + format.format(new Date()));
+        System.out.println("Starting Psimulator2, build " + format.format(
+                new Date()));
 
         // check jvm version  ... 1.7 or higher
-        Double jvmVersion = Double.parseDouble(System.getProperty("java.version").substring(0, 3));
-        
+        Double jvmVersion = Double.parseDouble(System.
+                getProperty("java.version").substring(0, 3));
+
         if (jvmVersion < 1.7) { // lower versions than JVM 1.7 aka JRE 7 are not supported
             System.err.println("Please install JRE 7 or higher.");
             System.exit(1);
@@ -50,23 +53,20 @@ public class Main {
         Logger.setLogger();
 
         if (args.length < 1) {
-/*
             Logger.log(Logger.ERROR, LoggingCategory.XML_LOAD_SAVE,
                     "No configuration file attached, run again with configuration file as first argument.");
-*/
-            //args = new String[] { "/home/roland/git/psimulator/examples/simple-network.xml" };
         }
 
         // Parse command line arguments
         configFileName = args[0];
-        
-        
+
         int firstTelnetPort = 11000;
         if (args.length >= 2) {
             try {
                 firstTelnetPort = Integer.parseInt(args[1]);
             } catch (NumberFormatException ex) {
-                Logger.log(Logger.WARNING, LoggingCategory.XML_LOAD_SAVE, "Second argument " + args[1] + " is not port number. Using default value: " + firstTelnetPort);
+                Logger.log(Logger.WARNING, LoggingCategory.XML_LOAD_SAVE,
+                        "Second argument " + args[1] + " is not port number. Using default value: " + firstTelnetPort);
             }
         }
 
@@ -75,20 +75,22 @@ public class Main {
             try {
                 eventServerPort = Integer.parseInt(args[2]);
             } catch (NumberFormatException ex) {
-                Logger.log(Logger.WARNING, LoggingCategory.XML_LOAD_SAVE, "Third argument " + args[2] + " is not port number. Using default value: " + eventServerPort);
+                Logger.log(Logger.WARNING, LoggingCategory.XML_LOAD_SAVE,
+                        "Third argument " + args[2] + " is not port number. Using default value: " + eventServerPort);
             }
         }
 
         // serializace xml do ukladacich struktur:
-        AbstractNetworkSerializer serializer = new NetworkModelSerializerXML();	// vytvori se serializer
+        AbstractNetworkSerializer serializer = new NetworkModelSerializerXML();
         NetworkModel networkModel = null;
         try {
-
-            networkModel = serializer.loadNetworkModelFromFile(new File(configFileName));	// nacita se xmlko do ukladacich struktur
-
+            networkModel = serializer.loadNetworkModelFromFile(new File(
+                    configFileName));
         } catch (SaveLoadException ex) {
-            Logger.log(Logger.DEBUG, LoggingCategory.XML_LOAD_SAVE, Util.stackToString(ex));
-            Logger.log(Logger.ERROR, LoggingCategory.XML_LOAD_SAVE, "Cannot load network model from: " + configFileName);
+            Logger.log(Logger.WARNING, LoggingCategory.XML_LOAD_SAVE, Util.
+                    stackToString(ex));
+            Logger.log(Logger.ERROR, LoggingCategory.XML_LOAD_SAVE,
+                    "Cannot load network model from: " + configFileName);
         }
 
         // nastaveni promennejch systemu pro telnet a pro ukladani:
@@ -96,24 +98,27 @@ public class Main {
         Psimulator.getPsimulator().configModel = networkModel;
         Psimulator.getPsimulator().lastConfigFile = configFileName;
 
-        // samotnej start systemu z ukladacich struktur
-        Loader loader = new Loader(networkModel, configFileName);	// vytvari se simulator loader
+        Loader loader = new Loader(networkModel, configFileName);
         loader.loadFromModel();	// simulator se startuje z tech ukladacich struktur
 
         // startovani telnetu:
         TelnetD telnetDaemon;
-        Logger.log(Logger.INFO, LoggingCategory.TELNET, "Starting telnet listeners");
+        Logger.log(Logger.INFO, LoggingCategory.TELNET,
+                "Starting telnet listeners");
         try {
 
-            telnetDaemon = TelnetD.createTelnetD(TelnetProperties.getProperties());
+            telnetDaemon = TelnetD.createTelnetD(TelnetProperties.
+                    getProperties());
             // @TODO pridat metodu na kontrolu obsazení portů
             telnetDaemon.start();
 
-            Logger.log(Logger.INFO, LoggingCategory.TELNET, "Telnet listeners successfully started");
+            Logger.log(Logger.INFO, LoggingCategory.TELNET,
+                    "Telnet listeners successfully started");
 
         } catch (BootException ex) {
             Logger.log(Logger.DEBUG, LoggingCategory.TELNET, ex.toString());
-            Logger.log(Logger.ERROR, LoggingCategory.TELNET, "Error occured when creating telnet servers.");
+            Logger.log(Logger.ERROR, LoggingCategory.TELNET,
+                    "Error occured when creating telnet servers.");
         }
 
         // SETUP EVENT SERVER
@@ -124,13 +129,15 @@ public class Main {
             if (Util.availablePort(eventServerPort)) {
                 break; // break while
             }
-            Logger.log(Logger.WARNING, LoggingCategory.EVENTS_SERVER, "Port " + eventServerPort + " not available, using:" + (eventServerPort + 1));
+            Logger.log(Logger.WARNING, LoggingCategory.EVENTS_SERVER,
+                    "Port " + eventServerPort + " not available, using:" + (eventServerPort + 1));
 
             tryMark++;
             eventServerPort++;
 
             if (tryMark > maxTryMark) {
-                Logger.log(Logger.ERROR, LoggingCategory.EVENTS_SERVER, "Cannot start event server, no port available");
+                Logger.log(Logger.ERROR, LoggingCategory.EVENTS_SERVER,
+                        "Cannot start event server, no port available");
             }
         }
 
@@ -142,7 +149,9 @@ public class Main {
 
         Logger.addListener(eventServer.getListener().getPacketTranslator());
 
-        Logger.log("PACKET FLOW SERVER: ", Logger.IMPORTANT, LoggingCategory.EVENTS_SERVER, "Server sucessfully started, listening on port: " + eventServerPort);
+        Logger.log("PACKET FLOW SERVER: ", Logger.IMPORTANT,
+                LoggingCategory.EVENTS_SERVER,
+                "Server sucessfully started, listening on port: " + eventServerPort);
 
     }
 }
